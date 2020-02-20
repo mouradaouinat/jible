b3811dc20b8098383154c88be9ef860a2d3b7212
Mourad Aouinat <mourad.eyes@gmail.com>
2020-02-20 11:13:25 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Ropa Sans", "sans-serif"],
+      display: ["Ropa Sans", "sans-serif"],
+      body: ["Ropa Sans", "sans-serif"]
+    },
+    colors: {
+      green: "#419D78"
+    }
+  },
+  variants: {},
+  plugins: []
+};
