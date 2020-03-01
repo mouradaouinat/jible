db6dc8a7e6206fbe59477e07ba8aefb9ddcd3967
Mourad Aouinat <mourad.eyes@gmail.com>
2020-03-01 08:02:33 +0100

    Fix too many modals bug

--- src/components/Adresses.jsx
+++ src/components/Adresses.jsx
@@ -10,9 +10,15 @@ const Adresses = () => {
   const { user, setUser } = useContext(UserContext);
   const [address, setAdress] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const [addressId, setAddressId] = useState("");
 
   function toggle() {
     setIsOpen(!isOpen);
+  }
+
+  function confirmDelete(id) {
+    setAddressId(id);
+    toggle();
   }
 
   function addAdress() {
@@ -62,48 +68,51 @@ const Adresses = () => {
           <Plus className="w-5 h-5" />
         </button>
       </div>
-      {user.addresses.map((address, idx) => (
-        <div className="mt-4" key={address.id}>
-          <h3 className="text-lg text-gray-300">Address #{idx + 1}</h3>
-          <div className="rounded border border-gray-100 p-2 flex justify-between items-center mt-2">
-            <div className="flex items-center">
-              <Pin className="w-5 h-5 mr-2" />
-              <p className="text-gray-300 text-xs lg:text-sm">
-                {address.address}
-              </p>
+      {user.addresses.length === 0 ? (
+        <div className="mt-4">
+          <p className="text-gray-300">You have no saved Addresses yet.</p>
+        </div>
+      ) : (
+        user.addresses.map((address, idx) => (
+          <div className="mt-4" key={address.id}>
+            <h3 className="text-lg text-gray-300">Address #{idx + 1}</h3>
+            <div className="rounded border border-gray-100 p-2 flex justify-between items-center mt-2">
+              <div className="flex items-center">
+                <Pin className="w-5 h-5 mr-2" />
+                <p className="text-gray-300 text-xs lg:text-sm">
+                  {address.address}
+                </p>
+              </div>
+              <button onClick={() => confirmDelete(address.id)}>
+                <Trash className="w-5 h-5 fill-current hover:text-red" />
+              </button>
             </div>
-            <button onClick={toggle}>
-              <Trash className="w-5 h-5" />
+            <div className="rounded overflow-hidden mt-2">
+              <img
+                src="https://www.google.com/maps/d/thumbnail?mid=1l4Clf7zV6PiFP7rAK0ML9Em8vTg&hl=en"
+                alt="map"
+                className="w-full h-24 object-cover object-center"
+              />
+            </div>
+          </div>
+        ))
+      )}
+      {isOpen && (
+        <Modal toggle={toggle}>
+          <p>Are you sure you want to delete this address?</p>
+          <div className="mt-4 text-right">
+            <button
+              className="text-white bg-red px-4 py-2 rounded mr-2"
+              onClick={() => deleteAddress(addressId)}
+            >
+              Delete
+            </button>
+            <button className="bg-gray-100 px-4 py-2 rounded" onClick={toggle}>
+              Cancel
             </button>
           </div>
-          <div className="rounded overflow-hidden mt-2">
-            <img
-              src="https://www.google.com/maps/d/thumbnail?mid=1l4Clf7zV6PiFP7rAK0ML9Em8vTg&hl=en"
-              alt="map"
-              className="w-full h-24 object-cover object-center"
-            />
-          </div>
-          {isOpen && (
-            <Modal toggle={toggle}>
-              <p>Are you sure you want to delete this address?</p>
-              <div className="mt-4 text-right">
-                <button
-                  className="text-white bg-red px-4 py-2 rounded mr-2"
-                  onClick={() => deleteAddress(address.id)}
-                >
-                  Delete
-                </button>
-                <button
-                  className="bg-gray-100 px-4 py-2 rounded"
-                  onClick={toggle}
-                >
-                  Cancel
-                </button>
-              </div>
-            </Modal>
-          )}
-        </div>
-      ))}
+        </Modal>
+      )}
     </div>
   );
 };
